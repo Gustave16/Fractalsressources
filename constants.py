f0c5327a6b1ed0c1bd53b1bdc37756c3a9ec77fd
Gustave16 <126259786+Gustave16@users.noreply.github.com>
2023-02-24 14:54:34 +0100

    python files to generate julia set
    you can use theses python code files to generate Julia sets

---
+++ constants.py
@@ -0,0 +1,15 @@
+Width = 1920
+Height = 1080
+
+max_x = 1.6
+max_y = 1.0
+offset_x = -1.6
+offset_y = -1.0
+
+borders = 1.0
+iterations = 100
+color_mode = 1
+complex_number = complex(-0.75, 0)
+Height = (max_y - offset_y) * float(Width) / (max_x - offset_x)
+Height = int(Height)
+size = Width, Height
